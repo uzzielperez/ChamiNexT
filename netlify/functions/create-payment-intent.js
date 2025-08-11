9316f3267edec86eec5fca39ec117fb3510c9ea7
Cilicia Uzziel Perez <uzzie.perez@cern.ch>
2025-08-12 00:51:54 +0200

    feat: add stripe payment intent function

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,22 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
